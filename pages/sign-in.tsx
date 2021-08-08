02ca1b5860fbe55b5a26a6fb2756e4ebd63beaee
Sharpedov <adr654231@gmail.com>
2021-08-08 09:54:15 +0200

    add redirect after sign in

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -2,11 +2,13 @@ import Head from "next/head";
 import React from "react";
 import SigninTemplate from "src/templates/signin";
 import { useAuth } from "src/components/authProvider";
+import { useRouter } from "next/router";
 
 export default function SignIn() {
 	const { redirectIfLogged } = useAuth();
+	const router = useRouter();
 
-	redirectIfLogged("/");
+	redirectIfLogged(`${router.query.redirect ?? "/"}`);
 
 	return (
 		<>
