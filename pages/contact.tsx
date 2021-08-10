7786a2e89562a529153a6d4a7b50253d31d0d666
Adrian <adr654231@gmail.com>
2021-08-10 18:17:22 +0200

    add contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import React from "react";
+import styled from "styled-components";
+
+export default function Contact({}) {
+	return (
+		<>
+			<Head>
+				<title>Contact - Eshop</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Container>
+				<Wrapper>
+					<Heading>
+						<h2>Contact</h2>
+					</Heading>
+				</Wrapper>
+			</Container>
+		</>
+	);
+}
+
+const Container = styled.div`
+	position: relative;
+	background: ${({ theme }) => theme.surface.primary};
+	min-height: 70vh;
+	padding: 20px 15px;
+
+	@media (min-width: 480px) {
+		padding: 20px 20px;
+	}
+`;
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	max-width: 1440px;
+	width: 100%;
+	margin: 0 auto;
+
+	> img {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		display: block;
+		width: 90%;
+		height: 300px;
+		object-fit: contain;
+	}
+
+	@media (min-width: 768px) {
+		> img {
+			width: 40%;
+		}
+	}
+`;
+
+const Heading = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+
+	> h2 {
+		font-size: ${({ theme }) => `calc(${theme.font.xs} + 3vw)`};
+	}
+`;
